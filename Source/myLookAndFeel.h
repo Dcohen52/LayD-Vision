5f128eefc2c581a893beac93187c452bdd9b5cfe
Dekel Cohen <dcohen52@gmail.com>
2021-10-28 09:31:11 +0300

    2nd Clean

--- Source/myLookAndFeel.h
+++ Source/myLookAndFeel.h
@@ -12,21 +12,6 @@
 
 #include <JuceHeader.h>
 
-//==============================================================================
-/*
-*/
-//class myLookAndFeel  : public juce::Component
-//{
-//public:
-//    myLookAndFeel();
-//    ~myLookAndFeel() override;
-//
-//    void paint (juce::Graphics&) override;
-//    void resized() override;
-//
-//private:
-//    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (myLookAndFeel)
-//};
 
 using namespace juce;
 
@@ -43,7 +28,6 @@ private:
 
 };
 
-////==============================================================================
 class myLookAndFeelV2 : public LookAndFeel_V4
 {
 
@@ -58,17 +42,3 @@ private:
 
 };
 
-////==============================================================================
-//class myLookAndFeelV3 : public LookAndFeel_V4
-//{
-//
-//public:
-//    myLookAndFeelV3();
-//
-//    void drawRotarySlider(Graphics& g, int x, int y, int width, int height, float sliderPos,
-//        float rotaryStartAngle, float rotaryEndAngle, Slider& slider) override;
-//
-//private:
-//    Image img2;
-//
-//};
